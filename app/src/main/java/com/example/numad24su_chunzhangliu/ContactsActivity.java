ba0935b6b5edef71e6f5242b1648533ce6490f3e
jimyh <jimyhacker@outlook.com>
2025-02-22 21:03:31 -0800

    A4:add contacts layout

--- app/src/main/java/com/example/numad24su_chunzhangliu/ContactsActivity.java
+++ app/src/main/java/com/example/numad24su_chunzhangliu/ContactsActivity.java
@@ -3,12 +3,23 @@ package com.example.numad24su_chunzhangliu;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ContactsActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private FloatingActionButton fab;
+    private List<String> contactList = new ArrayList<>(); // contacts information
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacts);
     }
 }
 
